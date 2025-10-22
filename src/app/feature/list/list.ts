e42db40ed7bd12efeab4305a046f65827b384c4b
Pedro Trillo <martin.trillo.c@gmail.com>
2025-10-22 12:23:22 -0500

    se agrego categorias y descuento

--- src/app/feature/list/list.ts
+++ src/app/feature/list/list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgStyle, NgClass,CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 interface Producto{
   id:number;
@@ -7,23 +7,48 @@ interface Producto{
   precio:number;
   estado:"Disponible"|"Agotado"|"En Oferta"
   imagen:string;
+  categoria:number;
+}
+interface Categoria{
+  id:number;
+  nombre:string;
+  descuento:number;
 }
 
 @Component({
   selector: 'app-list',
-  imports: [NgStyle, NgClass,CommonModule],
+  imports: [CommonModule],
   templateUrl: './list.html',
   styleUrl: './list.css'
 })
 export class List {
-  productos:Producto[]=[
-    {id:0,nombre:"Televisión",precio:100,estado:"En Oferta",imagen:"/imgElectronics/tv.webp"},
-    {id:1,nombre:"Lavadora",precio:200,estado:"Disponible",imagen:"/imgElectronics/lavadora.webp"},
-    {id:2,nombre:"Plancha",precio:70,estado:"En Oferta",imagen:"/imgElectronics/plancha.webp"},
-    {id:3,nombre:"Monitor",precio:150,estado:"En Oferta",imagen:"/imgElectronics/monitor.webp"},
-    {id:4,nombre:"Smartphone",precio:400,estado:"Disponible",imagen:"/imgElectronics/smartphone.webp"},
-    {id:5,nombre:"Audifonos",precio:90,estado:"Agotado",imagen:"/imgElectronics/audifonos.webp"}
+  descuento:number=0.10;
+   categorias:Categoria[]=[
+    {id:1,nombre:"Línea Blanca",descuento:0.10},
+    {id:2,nombre:"Electrónica",descuento:0.15}
   ]
+
+  productos:Producto[]=[
+    {id:0,nombre:"Televisión",precio:100,estado:"En Oferta",imagen:"/imgElectronics/tv.webp",categoria:2},
+    {id:1,nombre:"Lavadora",precio:200,estado:"En Oferta",imagen:"/imgElectronics/lavadora.webp",categoria:1},
+    {id:2,nombre:"Plancha",precio:70,estado:"En Oferta",imagen:"/imgElectronics/plancha.webp",categoria:1},
+    {id:3,nombre:"Monitor",precio:150,estado:"En Oferta",imagen:"/imgElectronics/monitor.webp",categoria:2},
+    {id:4,nombre:"Smartphone",precio:400,estado:"Disponible",imagen:"/imgElectronics/smartphone.webp",categoria:2},
+    {id:5,nombre:"Audifonos",precio:90,estado:"Agotado",imagen:"/imgElectronics/audifonos.webp",categoria:2}
+  ]
+ 
+ 
+  obtenerDescuentoCategoria(categoriaId: number): number {
+    const categoria = this.categorias.find(cat => cat.id === categoriaId);
+    return categoria ? categoria.descuento : 0;
+  }
+
+ 
+  calcularPrecioConDescuento(producto: Producto): number {
+    const descuento = this.obtenerDescuentoCategoria(producto.categoria);
+    return producto.precio * (1 - descuento);
+  }
+
   ngOnInit(){
     console.log("Componente inicializado");
   }
