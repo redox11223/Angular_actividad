d62ad65bd91d3466fef98f7b5841a539e8e59542
Miguel Angel <miguelgarcia241296@gmail.com>
2025-10-21 22:41:32 -0500

    Cambie la disponibilidad de vista de los productos

--- src/app/feature/list/list.ts
+++ src/app/feature/list/list.ts
@@ -1,28 +1,28 @@
 import { Component } from '@angular/core';
-import { NgStyle, NgClass } from '@angular/common';
+import { NgStyle, NgClass,CommonModule} from '@angular/common';
 
 interface Producto{
   id:number;
   nombre:string;
   precio:number;
-  agotado:boolean
+  estado:"Disponible"|"Agotado"|"En Oferta"
   imagen:string;
 }
 
 @Component({
   selector: 'app-list',
-  imports: [NgStyle, NgClass],
+  imports: [NgStyle, NgClass,CommonModule],
   templateUrl: './list.html',
   styleUrl: './list.css'
 })
 export class List {
   productos:Producto[]=[
-    {id:0,nombre:"Televisión",precio:100,agotado:true,imagen:"/imgElectronics/tv.webp"},
-    {id:1,nombre:"Lavadora",precio:200,agotado:true,imagen:"/imgElectronics/lavadora.webp"},
-    {id:2,nombre:"Plancha",precio:70,agotado:false,imagen:"/imgElectronics/plancha.webp"},
-    {id:3,nombre:"Monitor",precio:120,agotado:false,imagen:"/imgElectronics/monitor.webp"},
-    {id:4,nombre:"Smartphone",precio:400,agotado:false,imagen:"/imgElectronics/smartphone.webp"},
-    {id:5,nombre:"Audifonos",precio:90,agotado:true,imagen:"/imgElectronics/audifonos.webp"}
+    {id:0,nombre:"Televisión",precio:100,estado:"En Oferta",imagen:"/imgElectronics/tv.webp"},
+    {id:1,nombre:"Lavadora",precio:200,estado:"Disponible",imagen:"/imgElectronics/lavadora.webp"},
+    {id:2,nombre:"Plancha",precio:70,estado:"En Oferta",imagen:"/imgElectronics/plancha.webp"},
+    {id:3,nombre:"Monitor",precio:150,estado:"En Oferta",imagen:"/imgElectronics/monitor.webp"},
+    {id:4,nombre:"Smartphone",precio:400,estado:"Disponible",imagen:"/imgElectronics/smartphone.webp"},
+    {id:5,nombre:"Audifonos",precio:90,estado:"Agotado",imagen:"/imgElectronics/audifonos.webp"}
   ]
   ngOnInit(){
     console.log("Componente inicializado");
